d911b449f1555d9b2102ee011c8b43e25713847a
Peter Njuguna <petermwaurapeter254@gmail.com>
2023-03-30 14:13:16 +0300

    fetcher

--- src/components/fetcher.ts
+++
@@ -1,7 +0,0 @@
-import { request, RequestDocument, Variables } from "graphql-request";
-export const Fetcher = (
-  query: RequestDocument,
-  variables?: Variables | undefined
-) => {
-  return request("/api/graphql", query, variables);
-};
