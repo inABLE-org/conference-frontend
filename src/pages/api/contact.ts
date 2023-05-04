62d709198fdb2125e549a262bae33a1ef5641e7b
Peter Njuguna <peter.njuguna@technopriseglobal.com>
2023-05-04 17:08:54 +0300

    Fixed environmrntal variables

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { transporter, mailOptions } from "../../../nodemailerconfig"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method == "POST") {
+    const data = req.body
+    try {
+      await transporter.sendMail({
+        ...mailOptions,
+        text: "This is a test String",
+        subject:"Inclusive Africa (Contact us Page)",
+        html: `<h1>Email from Inclusive Africa Contact us Page<h1><br>
+               <p>Name: ${data.name}</p>
+               <p>Email: ${data.email}</p>
+               <p>Message: ${data.message}</p>`
+      })
+      res.status(400).json({ message: "Thanks Your Message was sent Successfully" })
+    } catch (error: any) {
+      if (typeof error.message === 'string') {
+        res.status(400).json({ message: "Error occured" })
+      }
+    }
+  } else {
+    res.status(400).json({ message: "Invalid Request" });
+  }
+}
+
