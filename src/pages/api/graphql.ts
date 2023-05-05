19f769175a15380914ee33b9e937b0a943501d14
Peter Njuguna <peter.njuguna@technopriseglobal.com>
2023-05-05 09:43:12 +0300

    code cleanup

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -15,6 +15,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     res.status(200).json(response.data);
   } catch (e) {
-    console.log(e);
+    res.status(200).json(e);
   }
 }
